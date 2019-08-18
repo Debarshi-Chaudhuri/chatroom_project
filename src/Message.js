0a530e760350cac46cca3e98cbc8c9a6ffd4df92
Debarshi-Chaudhuri <dchaudhuri17@gmail.com>
2019-08-18 12:59:39 +0530

    first commit

--- src/Message.js
+++ src/Message.js
@@ -1,20 +1,28 @@
 import React,{Component} from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import './App.css'
+
 class Message extends Component{
+   
     render(){
+        
         if(this.props.fromMe)
         return(
-        <div style={{textAlign:'right'}}>
-            <p>{this.props.name} : {this.props.message}</p>
+        <div >
+            <ul>
+            <p className='right'><Avatar style={{marginLeft:'92%',backgroundColor:'orange',width:'30px',height:'30px',fontSize:'15px'}}>{this.props.name.substring(0,3)}</Avatar>{this.props.message}</p>  
             
+            </ul>          
         </div>
-        
         );
         else
         return(
-        <div style={{textAlign:'left'}}>
-            <p>{this.props.name} : {this.props.message}</p>
+            <div >
+            <ul>
+            <p className='left'><Avatar style={{backgroundColor:'purple',width:'30px',height:'30px',fontSize:'15px'}}>{this.props.name.substring(0,3)}</Avatar>{this.props.message}</p>  
             
-        </div>    
+            </ul>          
+        </div>
         )
     }
 }
